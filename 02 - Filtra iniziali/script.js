e60756c697e3825207e55150e2848c65d3ffc3da
davidetoscano129 <toscano.davidew@gmail.com>
2024-11-06 16:44:43 +0100

    Es 02 Filtra iniziali Completato

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,14 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filterByInitial(array, letter) {
+    // Utilizzo filter per creare un nuovo array contenente solo le parole che iniziano con la lettera specificata
+    return array.filter(name => name[0].toUpperCase() === letter.toUpperCase());
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const filteredNames = filterByInitial(names, "A");
+console.log(filteredNames);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
